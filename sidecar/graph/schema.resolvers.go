5550d1de46125c9430d10e17a9360bcd91f6165f
難波 智貴 <1113053@PC1212.local>
2023-02-01 15:32:30 +0900

    resolver 実装

--- sidecar/graph/schema.resolvers.go
+++ sidecar/graph/schema.resolvers.go
@@ -6,25 +6,68 @@ package graph
 import (
 	"context"
 	"fmt"
+	"log"
 	"sidecar/graph/generated"
 	"sidecar/graph/model"
 )
 
-// CreateTodo is the resolver for the createTodo field.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
-}
+// GetChatUsers is the resolver for the getChatUsers field.
+func (r *queryResolver) GetChatUsers(ctx context.Context, input int) ([]*model.ChatUsersResponse, error) {
+	rows, err := r.db.Query(`
+			select
+				u.id,
+				u.name,
+				latest_messages.message
+			from
+			friends f
+			inner join users u on
+				u.id = f.friend_id
+			inner join
+			(
+			select
+			*
+			from
+			direct_comments
+			where
+			not exists (
+				select
+				1
+				from
+				direct_comments sub
+				where
+				direct_comments.room_id = sub.room_id
+				and direct_comments.created_at < sub.created_at
+			)
+			) latest_messages
+				on
+				f.room_id = latest_messages.room_id
+			and f.user_id = 1;`)
+	if err != nil {
+		log.Fatalf("getRows db.Query error err:%v", err)
+		return nil, err
+	}
+	defer rows.Close()
 
-// Todos is the resolver for the todos field.
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
-}
+	var userChats []*model.ChatUsersResponse
 
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+	for rows.Next() {
+		u := model.ChatUsersResponse{}
+		user := model.User{}
+		if err := rows.Scan(&u.User.ID, &user.Name, &u.LatestMessage); err != nil {
+			log.Fatalf("getRows rows.Scan error err:%v", err)
+		}
+		u.User = &user
+		fmt.Println(u.User.Name)
+		userChats = append(userChats, &model.ChatUsersResponse{
+			User:          &user,
+			LatestMessage: u.LatestMessage,
+		})
+	}
+
+	return userChats, nil
+}
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
